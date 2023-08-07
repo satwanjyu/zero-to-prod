0cb9b19f6d36ec300e0d794cfe49e24a1711c71f
失魂魚 <satwanjyu@outlook.com>
2023-08-07 18:11:09 +0800

    7.3

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,18 @@
 #![warn(clippy::pedantic)]
 
-use std::net::TcpListener;
-
-use sqlx::postgres::PgPoolOptions;
-use zero_to_prod::{
-    configuration::get_configuration,
-    startup,
-    telemetry::{get_subscriber, init_subscriber},
-};
+use zero_to_prod::telemetry;
+use zero_to_prod::{configuration::get_configuration, startup::Application};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero-to-prod".into(), "info".into(), std::io::stdout);
-    init_subscriber(subscriber);
+    let subscriber =
+        telemetry::get_subscriber("zero-to-prod".into(), "info".into(), std::io::stdout);
+    telemetry::init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPoolOptions::new()
-        .acquire_timeout(std::time::Duration::from_secs(2))
-        .connect_lazy_with(configuration.database.with_db());
-    let listener = TcpListener::bind(format!(
-        "{}:{}",
-        &configuration.application.host, &configuration.application.port
-    ))
-    .expect("Failed to bind to address");
 
-    startup::run(listener, connection_pool)?.await
+    let application = Application::build(configuration).await?;
+    application.run_until_stopped().await?;
+
+    Ok(())
 }
